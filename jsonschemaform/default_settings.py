301be0f7c0679dd7a8170fc4c483d9da097c0f88
Tessa Alexanian <tessa@zymergen.com>
2020-05-04 12:49:09 -0700

    Use json-editor v2.2.1 from a CDN

--- jsonschemaform/default_settings.py
+++ jsonschemaform/default_settings.py
@@ -1,47 +1,48 @@
 settings = {
-    'css': {
-        'all': (
-            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-        )
-    },
-    'options': {
-        'theme': 'bootstrap3',
-        'iconlib': 'bootstrap3',
-        'no_additional_properties': True,
+    "options": {
+        "theme": "bootstrap4",
+        "iconlib": "fontawesome5",
+        "no_additional_properties": True,
     }
 }
 
 
 """
 The default configuration can be overriten in django settings:
-To a better undestanding on this options plese refer to: https://github.com/jdorn/json-editor#options
+To a better undestanding on this options plese refer to: https://github.com/json-editor/json-editor#options
 """
-
 # JSONSCHEMAFORM = {
-#     'css': {
-#         'all': (
-#             'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-#         )
+#     "options": {
+#         "ajax": False,
+#         "ajaxBase": "",
+#         "ajaxCredentials": False,
+#         "compact": False,
+#         "disable_array_add": False,
+#         "disable_array_delete": False,
+#         "disable_array_delete_all_rows": False,
+#         "disable_array_delete_last_row": False,
+#         "disable_array_reorder": False,
+#         "enable_array_copy": False,
+#         "disable_collapse": False,
+#         "disable_edit_json": False,
+#         "disable_properties": False,
+#         "array_controls_top": False,
+#         "form_name_root": 'root',
+#         "iconlib": None,
+#         "remove_button_labels": False,
+#         "no_additional_properties": False,
+#         "refs": {},
+#         "required_by_default": False,
+#         "keep_oneof_values": True,
+#         "schema": {},
+#         "show_errors": "interaction",
+#         "startval": None,
+#         "template": "default",
+#         "theme": "html",
+#         "display_required_only": False,
+#         "show_opt_in": False,
+#         "prompt_before_delete": True,
+#         "object_layout": "normal",
+#         "enum_source_value_auto_select": True,
 #     },
-#     'options': {
-#         'ajax': False,
-#         'disable_array_add': False,
-#         'disable_array_delete': False,
-#         'disable_array_reorder': False,
-#         'disable_collapse': False,
-#         'disable_edit_json': False,
-#         'disable_properties': False,
-#         'form_name_root': 'root',
-#         'iconlib': None,
-#         'no_additional_properties': False,
-#         'refs': {},
-#         'required_by_default': False,
-#         'keep_oneof_values': True,
-#         'schema': {},
-#         'show_errors': "interaction",
-#         'startval': None,
-#         'template': 'default',
-#         'theme': 'html',
-#         'display_required_only': False
-#     }
 # }
